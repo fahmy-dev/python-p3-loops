4993628bff3af5a0e08d0add679003a1d43de400
fahmy-dev <faahmy@proton.me>
2025-02-13 11:53:30 +0300

    initial commit

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,36 @@
 #!/usr/bin/env python3
 
+import ipdb
+
 def happy_new_year():
-    # code goes here!
-    pass
+    i = 10
+    
+    while i > 0:
+        print(i)
+        i -= 1
+
+    print("Happy New Year!")
+
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    squared_integers = [num * num for num in int_list]
+    return squared_integers
+
+square_integers([1, 2, 3, 4])
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    i = 1
+    while i <= 100:
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+        
+        i += 1
+        
+
+fizzbuzz()
